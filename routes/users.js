640c65f3daa3e1f0c6f4dd6a4561160f73302b36
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-12-11 21:56:37 +0700

    tambah verify token

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 const userHandler = require("./handlers/users");
 const uploadImage = require("../middleware/uploadImage");
+const verify = require("../middleware/verify-token");
 
 // Get all users data
 router.get("/", userHandler.getAllUsers);
 router.post("/", userHandler.createUser);
-router.put("/:id", uploadImage.single("image_profile"), userHandler.updateUser);
+router.put(
+  "/:id",
+  verify,
+  uploadImage.single("image_profile"),
+  userHandler.updateUser
+);
 router.delete("/:id", userHandler.deleteUser);
 
 module.exports = router;
